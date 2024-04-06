83e9a5de571a01113664cdb804a66996b2373e6e
gururandy <gururandy239@gmail.com>
2024-04-06 21:27:35 +0530

    config todo project

---
+++ database/migrations/2024_04_06_153150_create_todolist_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todolist_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->text('description');
+            $table->string('completed_status')->nullable()->default('pending');
+            $table->dateTime('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todolist_details');
+    }
+};
